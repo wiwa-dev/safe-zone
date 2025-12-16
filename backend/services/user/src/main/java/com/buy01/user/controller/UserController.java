f1f2f4d3cc24f151053be6d4244d1c0a7c9f83aa
wiwa-dev <63481064+wiwa-dev@users.noreply.github.com>
2025-12-16 13:32:07 +0000

    Clean up UserController by removing comment
    Removed unnecessary comment line in UserController.

--- backend/services/user/src/main/java/com/buy01/user/controller/UserController.java
+++ backend/services/user/src/main/java/com/buy01/user/controller/UserController.java
@@ -20,7 +20,7 @@ import com.buy01.user.service.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-//
+//controller
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
