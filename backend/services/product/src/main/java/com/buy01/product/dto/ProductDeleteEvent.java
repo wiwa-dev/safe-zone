989f09a77db730fde5d4a54e0fe1d5f19a83c4f4
wiwa-dev <63481064+wiwa-dev@users.noreply.github.com>
2025-12-19 12:48:53 +0000

    test5

--- backend/services/product/src/main/java/com/buy01/product/dto/ProductDeleteEvent.java
+++ backend/services/product/src/main/java/com/buy01/product/dto/ProductDeleteEvent.java
@@ -12,7 +12,4 @@ public class ProductDeleteEvent {
     private String productId;
     private String eventType;
 
-     public Boolean isSameNumberValue(AtomicLong a, AtomicLong b) {
-        return a.equals(b); // Noncompliant, this is true only if a == b.....
-    }
 }
