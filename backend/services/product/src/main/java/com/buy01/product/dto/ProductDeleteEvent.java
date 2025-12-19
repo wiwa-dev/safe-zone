66a2db44517c259c58a73e04b94e0a00fc97709b
wiwa-dev <63481064+wiwa-dev@users.noreply.github.com>
2025-12-19 12:34:10 +0000

    media product test

--- backend/services/product/src/main/java/com/buy01/product/dto/ProductDeleteEvent.java
+++ backend/services/product/src/main/java/com/buy01/product/dto/ProductDeleteEvent.java
@@ -1,13 +1,18 @@
 package com.buy01.product.dto;
 
 import lombok.AllArgsConstructor;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDeleteEvent {
     private String productId;
     private String eventType;
+
+     public Boolean isSameNumberValue(AtomicLong a, AtomicLong b) {
+        return a.equals(b); // Noncompliant, this is true only if a == b.....
+    }
 }
